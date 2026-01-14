248bbcce740f615b3ea8f7fee67e400e139428d4
NESAT2822 <fatemanesat968@gmail.com>
2026-01-15 01:22:19 +0600

    Add files via upload

---
+++ Availability.cs
@@ -0,0 +1,15 @@
+﻿namespace DoctorAppoinmentSystem.Models
+{
+    public class Availability
+    {
+        public int Id { get; set; }
+        public int DoctorProfileId { get; set; }
+        public required DoctorProfile Doctor { get; set; }
+
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
