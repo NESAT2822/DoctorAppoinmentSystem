806c14f7858b1ba48226e5889b8062fcf5570dbb
NESAT2822 <fatemanesat968@gmail.com>
2026-01-15 01:23:29 +0600

    Add files via upload

---
+++ Specialty.cs
@@ -0,0 +1,10 @@
+﻿namespace DoctorAppoinmentSystem.Models
+{
+    public class Specialty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public ICollection<DoctorProfile> Doctors { get; set; } = new List<DoctorProfile>();
+    }
+}
